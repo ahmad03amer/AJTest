5cf36a3952062d6da90f89572cd401615b5b2821
Ahmad Amer <a7mad.amerr@gmail.com>
2021-02-27 19:20:24 +0200

    upload project , first commit

---
+++ src/main/java/com/accessjobs/pjp/PjpApplication.java
@@ -0,0 +1,13 @@
+package com.accessjobs.pjp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjpApplication.class, args);
+	}
+
+}
